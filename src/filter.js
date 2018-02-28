8f7a28f06a991b2e69dc83011414318e08989130
Alec M <alecmcgovern444@gmail.com>
2018-02-28 18:49:41 -0500

    basic animation and textures

--- src/filter.js
+++ src/filter.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import * as PIXI from 'pixi.js';
+import * as Filters from 'pixi-filters';
 
 import mars from './images/mars.png';
 import './filter.css';
@@ -9,6 +10,7 @@ class Filter extends React.Component {
 		super(props);
 		
 		this.animate = this.animate.bind(this);
+		this.setup = this.setup.bind(this);
 		this.marsImage = mars;
 	}
 
@@ -17,17 +19,29 @@ class Filter extends React.Component {
 		this.app.renderer.backgroundColor = 0x7F0000FF;
 		this.refs.filter.appendChild(this.app.view);
 
-		this.app.stage.width = 500;
-		this.app.stage.height = 375;
+		PIXI.loader.add([this.marsImage]).load(this.setup);
 
-		PIXI.loader.add([this.marsImage]).load(() => {
-			this.sprite1 = new PIXI.Sprite(
+		this.filter = new Filters.DotFilter();
+
+		// this.animate();
+	}
+
+	setup() {
+		this.sprite1 = new PIXI.Sprite(
 				PIXI.loader.resources[this.marsImage].texture
 			);
-			this.app.stage.addChild(this.sprite1);
-		});
+		this.app.stage.addChild(this.sprite1);
+
+		this.sprite1.position.set(0, 100);
+		this.sprite1.scale.set(2,2);
+		this.sprite1.filters = [this.filter];
+
+		// this.app.ticker.add(delta => this.gameLoop(delta));
 
 		this.animate();
+	}
+
+	gameLoop(delta) {
 	}
 
 	componentWillUnmount() {
@@ -35,6 +49,9 @@ class Filter extends React.Component {
 	}
 
 	animate() {
+		if (this.sprite1.x < 200) {
+			this.sprite1.x += 1;
+		}
 		this.app.render(this.app.stage);
 		this.frame = requestAnimationFrame(this.animate);
 	}
